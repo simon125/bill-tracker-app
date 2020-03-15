6d170ca8de953cab88c2f523616a085b3d80a483
Szymon <szymqwerty@gmail.com>
2020-03-15 18:49:45 +0100

    BT007 added couple dependencies for backend

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('client/build'));
 
-app.get('/test', (req, res) => {
-  res.status(200).json({ msg: 'You did it!' });
-});
+console.log(123);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
