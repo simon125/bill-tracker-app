e8de6b46f943ab1bcf3bb84b5740adb255c051c2
Szymon <szymqwerty@gmail.com>
2020-03-15 14:45:42 +0100

    006 Added some changes for test builds

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('client/build'));
 
+app.get('/test', (req, res) => {
+  res.status(200).json({ msg: 'You did it!' });
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
