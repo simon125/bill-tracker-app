6e22a44bb93d9bc3005a5de6587b04745a00eae9
Szymon <szymqwerty@gmail.com>
2020-03-15 19:30:15 +0100

    BT007 work on api/v1/bills route
    - added bodyparser to project
    - created placeholders routes for further work

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import billsRoute from './routes/bills';
 
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static('client/build'));
+app.use(bodyParser.json());
 
-console.log(123);
+app.use('/api/v1/bills', billsRoute);
+
+app.use(express.static('client/build'));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
---
+++ src/routes/bills.ts
@@ -0,0 +1,70 @@
+import express from 'express';
+
+const router = express.Router();
+
+// @route  GET api/v1/bills
+// @desc   Get all users bills
+// @access Private
+
+router.get('/', async (req, res) => {
+  try {
+    res.status(200).json({ msg: 'Jupi you did it!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/v1/bills/:id
+// @desc   Get users bill by id
+// @access Private
+
+router.get('/:id', async (req, res) => {
+  try {
+    res.status(200).json({ msg: 'Jupi you did it!', id: req.params.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  POST api/v1/bills
+// @desc   Add bill
+// @access Private
+
+router.post('/', async (req, res) => {
+  try {
+    res.status(200).json({ youWillSaveIt: req.body });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  PUT api/v1/bills/:id
+// @desc   Update bill
+// @access Private
+
+router.put('/:id', async (req, res) => {
+  try {
+    res.status(200).json({ updatedthings: req.body, id: req.params.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  DELETE api/v1/bills/:id
+// @desc   Delete bill
+// @access Private
+
+router.delete('/:id', async (req, res) => {
+  try {
+    res.status(200).json({ idToDelete: req.params.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+export default router;
