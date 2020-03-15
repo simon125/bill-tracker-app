2a6a0b9e36c85ce04425702c3bf72579302594de
Szymon <szymqwerty@gmail.com>
2020-03-15 15:50:07 +0100

    WORKING CHECKPOINT

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
 app.get('/test', (req, res) => {
   res.status(200).json({ msg: 'You did it!' });
