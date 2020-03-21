226ad3641bb7be33b71b3484fdb5f044ada7a1d0
Szymon <szymqwerty@gmail.com>
2020-03-21 14:49:14 +0100

    BT014 added antdesign and react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <Counter />
         <p></p>
       </header>
